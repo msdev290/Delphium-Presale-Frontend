73991dcff9094885f3ac867bd563f56a39cca776
mind1995star <daisukenagata0209@gmail.com>
2023-10-13 06:27:28 +0900

    update: add input for token exchange

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,7 @@ const Home: NextPage = () => {
   const [isConnect, setIsConnect] = useState(false);
   const [payableAmount, setPayableAmount] = useState("0");
   const [userBalance, setUserBalance] = useState(0);
+  const [getPhium, setGetPhium] = useState(0);
   const currentDate = new Date();
   const timeStamp = Math.floor(currentDate.getTime() / 1000);
   const [endTime, setEndTime] = useState(1705330800);
@@ -56,6 +57,12 @@ const Home: NextPage = () => {
   //       functionName: "sellAmount",
   //       chainId: 5,
   //     },
+  //     {
+  //       address: "0x5B54E62805FC3E502220a8D0F6dC40592AB4C7cD",
+  //       abi: Presaleabi,
+  //       functionName: "rate",
+  //       chainId: 5,
+  //     },
   //   ],
   // });
 
@@ -73,11 +80,12 @@ const Home: NextPage = () => {
 
   // useEffect(() => {
   //   console.log("InitializeData:", Number(InitializeData[0].result));
+  //   console.log("InitializeData:", Number(InitializeData[4].result));
   // });
 
   const handleBuyToken = async () => {
     // BuyTokenWrite();
-    console.log("BUY", Math.floor(new Date().getTime() / 1000));
+    // console.log("BUY", Math.floor(new Date().getTime() / 1000));
   };
 
   const calcTime = () => {
@@ -122,6 +130,11 @@ const Home: NextPage = () => {
     return () => clearTimeout(timer);
   });
 
+  const onChangeUSDTValue = (e) => {
+    // setGetPhium(e.target.value / (Number(InitializeData[4].result) / 1000000));
+    setGetPhium(e.target.value / 0.002);
+  };
+
   return (
     <div>
       <div className="relative flex flex-col px-[280px] pt-4 justify-between h-screen">
@@ -133,7 +146,104 @@ const Home: NextPage = () => {
             </div>
           </Link>
           <div>
-            <ConnectButton />
+            <ConnectButton.Custom>
+              {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+              }) => {
+                // Note: If your app doesn't use authentication, you
+                // can remove all 'authenticationStatus' checks
+                const ready = mounted && authenticationStatus !== "loading";
+                const connected =
+                  ready &&
+                  account &&
+                  chain &&
+                  (!authenticationStatus ||
+                    authenticationStatus === "authenticated");
+
+                setUserBalance(account?.balanceFormatted);
+                // console.log("b:", account?.balanceFormatted);
+
+                return (
+                  <div
+                    {...(!ready && {
+                      "aria-hidden": true,
+                      style: {
+                        opacity: 0,
+                        pointerEvents: "none",
+                        userSelect: "none",
+                      },
+                    })}
+                  >
+                    {(() => {
+                      if (!connected) {
+                        return (
+                          <button
+                            onClick={openConnectModal}
+                            type="button"
+                            className="p-3 bg-[#ffffff24] rounded-2xl transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-[#4ea4ff] duration-300 shadow-2xl shadow-[#0e141a]"
+                          >
+                            Connect Wallet
+                          </button>
+                        );
+                      }
+
+                      if (chain.unsupported) {
+                        return (
+                          <button onClick={openChainModal} type="button">
+                            Wrong network
+                          </button>
+                        );
+                      }
+
+                      return (
+                        <div style={{ display: "flex", gap: 12 }}>
+                          <button
+                            onClick={openChainModal}
+                            style={{ display: "flex", alignItems: "center" }}
+                            type="button"
+                          >
+                            {chain.hasIcon && (
+                              <div
+                                style={{
+                                  background: chain.iconBackground,
+                                  width: 12,
+                                  height: 12,
+                                  borderRadius: 999,
+                                  overflow: "hidden",
+                                  marginRight: 4,
+                                }}
+                              >
+                                {chain.iconUrl && (
+                                  <img
+                                    alt={chain.name ?? "Chain icon"}
+                                    src={chain.iconUrl}
+                                    style={{ width: 12, height: 12 }}
+                                  />
+                                )}
+                              </div>
+                            )}
+                            {chain.name}
+                          </button>
+
+                          <button onClick={openAccountModal} type="button">
+                            {account.displayName}
+                            {account.displayBalance
+                              ? ` (${account.displayBalance})`
+                              : ""}
+                          </button>
+                        </div>
+                      );
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
             {/* <button className="px-[25px] py-[7px] bg-[#c5b939] text-[#fff] rounded-lg">
             Connect Wallet
           </button> */}
@@ -160,7 +270,7 @@ const Home: NextPage = () => {
           </div>
 
           <div className="flex justify-end w-1/2">
-            <div className="relative flex flex-col justify-center items-center gap-[30px] w-[500px] h-[380px] shadow-2xl shadow-[#0e141a] bg-[#33343B] px-[50px] py-[25px] rounded-xl">
+            <div className="relative flex flex-col justify-center items-center gap-[20px] w-[500px] h-[450px] shadow-2xl shadow-[#0e141a] bg-[#33343B] rounded-xl">
               <div className="absolute top-[-100px] left-[-50px]">
                 <Image src={Delphium} alt="Delphium" width={150} height={150} />
               </div>
@@ -211,7 +321,42 @@ const Home: NextPage = () => {
                   ></progress>
                 </div>
               </div>
-              <div>
+              <div className="flex flex-col gap-5 justify-center items-center">
+                {/* <div>
+                  <input></input> {"=>"} <input></input>
+                </div> */}
+                <div className="flex flex-row items-center justify-between w-full">
+                  <div className="form-control">
+                    {/* <label className="label">
+                    <span className="label-text text-white">Enter amount</span>
+                  </label> */}
+                    <label className="input-group">
+                      <input
+                        type="text"
+                        placeholder="0.01"
+                        onChange={(e) => onChangeUSDTValue(e)}
+                        className="input input-bordered text-black w-20"
+                      />
+                      <span className="text-black text-sm px-3">USDT</span>
+                    </label>
+                  </div>
+                  to
+                  <div className="form-control">
+                    {/* <label className="label">
+                    <span className="label-text text-white">Enter amount</span>
+                  </label> */}
+                    <label className="input-group">
+                      <input
+                        type="text"
+                        placeholder="0.01"
+                        value={getPhium}
+                        className="input input-bordered text-black w-20"
+                      />
+                      <span className="text-black text-sm px-3">$PHIUM</span>
+                    </label>
+                  </div>
+                </div>
+
                 <button
                   className="px-[25px] py-[7px] bg-[#4ea4ff] shadow-md shadow-[#0e141a] w-[350px] text-white rounded-lg"
                   onClick={handleBuyToken}
